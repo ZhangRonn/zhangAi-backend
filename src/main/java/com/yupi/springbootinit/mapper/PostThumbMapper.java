de302a6c12135d30dc8373eddb63b542df208a8f
“ZhangRonn” <2830913880@qq.com>
2024-04-27 14:52:12 +0800

    a

---
+++ src/main/java/com/yupi/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,16 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,27 @@
+package com.yupi.springbootinit.model.dto.postfavour;
+
+import com.yupi.springbootinit.common.PageRequest;
+import com.yupi.springbootinit.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
